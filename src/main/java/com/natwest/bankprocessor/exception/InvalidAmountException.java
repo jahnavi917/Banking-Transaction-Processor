677a58e831b5a0057a7b5e6bf6d8acda7d63d166
jahnavi917 <vjahnavi01751a1218@gmail.com>
2026-07-06 21:31:12 +0530

    Add InvalidAmountException class for error handling

--- src/main/java/com/natwest/bankprocessor/exception/InvalidAmountException.java
+++ src/main/java/com/natwest/bankprocessor/exception/InvalidAmountException.java
@@ -1,1 +1,5 @@
-
+// InvalidAmountException.java
+package com.natwest.bankprocessor.exception;
+public class InvalidAmountException extends RuntimeException {
+    public InvalidAmountException(String msg) { super(msg); }
+}
