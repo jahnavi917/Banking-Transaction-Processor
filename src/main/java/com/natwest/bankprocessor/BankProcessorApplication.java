0c9721ad0dcc156397db2064f3d9f55a7acaa37a
jahnavi917 <vjahnavi01751a1218@gmail.com>
2026-07-06 21:39:07 +0530

    Initialize BankProcessorApplication with Spring Boot

--- src/main/java/com/natwest/bankprocessor/BankProcessorApplication.java
+++ src/main/java/com/natwest/bankprocessor/BankProcessorApplication.java
@@ -1,1 +1,11 @@
+package com.natwest.bankprocessor;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankProcessorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankProcessorApplication.class, args);
+    }
+}
