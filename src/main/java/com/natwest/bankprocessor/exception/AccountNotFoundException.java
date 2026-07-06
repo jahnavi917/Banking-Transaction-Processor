c56df7397760e07e59b1a9c85cfa03cc5cf39b0c
jahnavi917 <vjahnavi01751a1218@gmail.com>
2026-07-06 21:29:30 +0530

    Add AccountNotFoundException for missing accounts

--- src/main/java/com/natwest/bankprocessor/exception/AccountNotFoundException.java
+++ src/main/java/com/natwest/bankprocessor/exception/AccountNotFoundException.java
@@ -1,1 +1,5 @@
-
+// AccountNotFoundException.java
+package com.natwest.bankprocessor.exception;
+public class AccountNotFoundException extends RuntimeException {
+    public AccountNotFoundException(String id) { super("Account with ID " + id + " was not located."); }
+}
